11249da6fda0292f79f5aabeafd796d2a67cd582
mikes94116 <mikes94116@yahoo.com>
2018-09-23 21:08:05 -0700

    readd

--- assets/javescript/app.js
+++ assets/javescript/app.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 
 var counter = 0;
-var timeLeft = 60;
+var timeLeft = 33;
+var correct
+var incorrect
 
 
 $(".quizReveal").hide();
@@ -11,34 +13,71 @@ $("button").click(function(){
     $(".welcome").hide();
 });
 
-var time = $("#timer");
-time.html(convertTime(timeLeft - counter));
-
-function convertTime(s) {
-
-    var min = floor(s/60);
-    //modulus...
-    var sec = s % 60;
-    return min + ":" + sec;
-
-}
+// var time = $("#timer");
 
 function timeIt() {
-    
+    counter;
     counter++;
-    time.html(convertTime(timeLeft - counter));
-
+    $("#timer").html(timeLeft - counter);
+    
 }
 
-setInterval(timeIt, 1000);
+var timerId = setInterval(timeIt, 1000)
 
-// function timer() {
+// after 10 seconds stop
+setTimeout(() => 
+{ clearInterval(timerId); 
+    alert('stop... time is up... you scored ' + correct + ' correct... and ' + incorrect + ' incorrect'); 
 
-//     time = Date.now();
-    
+    }, 
+    33000);
+
+
+$(function(){
+
+    correct = 0;
+    incorrect = 0;
+
+
+    $(".correct1").click(function() {
+    correct++;
+    });
+    $(".correct2").click(function() {
+    correct++;
+    });
+    $(".correct3").click(function() {
+    correct++;
+    });
+
+    $(".incorrect").click(function(){
+    incorrect++;
+    });
+});
+
+
+// setInterval(function() {
+//     counter++;
+//      if (counter >= 0) {
+//         time.html(timeLeft - counter);
+//      }
+//      if (counter === 0) {
+//         alert('sorry, out of time');
+//         clearInterval(counter);
+//       }
+//      1000;
+// });
+
+
+// correct += $('input[name="data[user_type]"]:radio').val();
+
+// function convertTime(s) {
+
+//     var min = math.floor(s/60);
+//     //modulus...
+//     var sec = s % 60;
+//     return min + ":" + sec;
+
 // }
-
-// console.log(time);
 
 
 });
